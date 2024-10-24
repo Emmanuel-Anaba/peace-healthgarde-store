9074dc08c318b52d5bfbec2866b7428269320e3e
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-10-24 01:52:09 +0100

    Added the border visiblity on scolling🙄

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,12 +6,32 @@ import Image from "next/image";
 import Link from "next/link";
 import { RiMenu3Line } from "react-icons/ri";
 import ThemeSwitcher from "./themeswitcher";
+import { useEffect, useRef } from "react";
 
 export default function Navbar() {
   const { navLinks, pathname, open, setOpen } = useAppContext();
+  const navbarRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        navbarRef.current?.classList.add("border-b-[1px]", "border-gray-400");
+      } else {
+        navbarRef.current?.classList.remove(
+          "border-b-[1px]",
+          "border-gray-400"
+        );
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <nav>
+    <nav ref={navbarRef}>
       <Image
         priority
         src={getImage("logo")}
